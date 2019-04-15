db8d69ad2c3e2877889274631aaf8b2435c87a1d
Jérôme BALIEUX <jerome.balieux@axiocode.com>
2019-04-15 15:12:36 +0200

    11. Register form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { RacesComponent } from './races/races.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 export const ROUTES = [
     { path: 'races', component: RacesComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '', component: HomeComponent },
     { path: '**', component: HomeComponent }
 ];
